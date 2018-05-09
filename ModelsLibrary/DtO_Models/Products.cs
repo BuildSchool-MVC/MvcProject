42f3c90e2da49837c4b5f0d97b54ffd203a82f07
A Kuan <s11996123@gmail.com>
2018-05-09 16:14:30 +0800

    Create Product.Shoppingcart

---
+++ ModelsLibrary/DtO_Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Products
+{
+    public int ProductID { get; set; }
+
+    public string ProductName { get; set; }
+
+    public int CategoryID { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public int UnitsInStock { get; set; }
+
+    public string Size { get; set; }
+
+    public DateTime Uptime { get; set; }
+
+    public DateTime Downtime { get; set; }
+}
---
+++ ModelsLibrary/DtO_Models/ShoppingcartDetails.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class ShoppingcartDetails
+{
+    public int CustomerID { get; set; }
+    public int ProductID { get; set; }
+    public int Quantity { get; set; }
+}

eb0f76dd1f95ef19af39c01c5f0d9c2857e92bf1
Chunhui <chunhui831211@gmail.com>
2018-05-13 14:03:50 +0800

    Add Customer Repository Test

---
+++ UnitTest/Repositorys/CustomerRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelsLibrary.Repository;
+
+namespace CustomerTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void GetAllTest()
+        {
+            var repository = new CustomerRepository();
+            var customer = repository.GetAll();
+            Assert.IsTrue(customer.Count() > 0);
+        }
+        [TestMethod]
+        public void FindById()
+        {
+            var repository = new CustomerRepository();
+            var customer = repository.FindById(1);
+            Assert.IsTrue(customer.CustomerID==1);
+        }
+        [TestMethod]
+        
+    }
+}
